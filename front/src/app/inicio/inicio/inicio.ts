47290f1444f5c63823493f2e38b946eebaaa4f95
eliespinozaa <eliespinoza.20189@gmail.com>
2025-09-23 22:05:35 -0600

    se crea cascaron

---
+++ front/src/app/inicio/inicio/inicio.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [],
+  templateUrl: './inicio.html',
+  styleUrl: './inicio.css',
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class Inicio {
+
+}
